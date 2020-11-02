986abc806ef2140b656678d19152a26728d89dc4
Wasim Akram <shuvokhan20885@gmail.com>
2020-11-02 12:27:11 +0600

    modify all files

---
+++ Interface/Interface/DemoClass.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Interface
+{
+    class DemoClass : FirstPerson, SecondPerson
+    {
+        public void firstMethod()
+        {
+            Console.WriteLine("Wasim Akram");
+        }
+        public void secondMethod()
+        {
+            Console.WriteLine("Shuvo");
+        }
+    }
+}
---
+++ Interface/Interface/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace Interface
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DemoClass name = new DemoClass();
+            name.firstMethod();
+            name.secondMethod();
+        }
+    }
+}
